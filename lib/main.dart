a2a82c168459981c0aba29291c9edf5319fc5a9a
“[buthaina <brbinhumaid@gmail.com>
2026-09-09 19:29:42 +0300

    .env update

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:project_1_flutter/screens/home_screen.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
+  await dotenv.load();
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
-    url: "https://owzictcomkslqcalehxw.supabase.co",
-    publishableKey: "ur key",
+    url: dotenv.get('my-url'),
+    publishableKey: dotenv.get('my-publishableKey'),
   );
   runApp(const MainApp());
 }
